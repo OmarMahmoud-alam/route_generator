ffefa83dc0f28a0c1800f691a82004b588e927df
omar208 <55194832+omar208@users.noreply.github.com>
2024-09-07 15:32:31 +0300

    first commit

---
+++ bin/route_generator.dart
@@ -0,0 +1,5 @@
+import 'package:route_generator/route_generator.dart' as route_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${route_generator.calculate()}!');
+}
---
+++ test/route_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:route_generator/route_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
